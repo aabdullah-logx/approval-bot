deb4b52a450f345725099e1bcea940f0c849b9ad
Ali Abdullah <aabdullah@logxgroup.com>
2026-04-03 00:28:36 +0500

    latest code

--- slack_utils.py
+++ slack_utils.py
@@ -10,21 +10,20 @@ def send_slack_message(message: str):
     if not settings.SLACK_BOT_TOKEN or not settings.SLACK_CHANNEL_ID:
         print("Warning: Slack Token or Channel ID is missing. Skipping notification.")
         return False
-    return None
+    
+    client = WebClient(token=settings.SLACK_BOT_TOKEN)
 
-    # client = WebClient(token=settings.SLACK_BOT_TOKEN)
-    #
-    # try:
-    #     response = client.chat_postMessage(
-    #         channel=settings.SLACK_CHANNEL_ID,
-    #         text=message
-    #     )
-    #     print("Slack notification sent successfully!")
-    #     return True
-    # except SlackApiError as e:
-    #     print(f"Error sending Slack notification: {e.response['error']}")
-    #     return False
-    # except Exception as e:
-    #     print(f"Unexpected error when sending Slack notification: {e}")
-    #     traceback.print_exc()
-    #     return False
+    try:
+        response = client.chat_postMessage(
+            channel=settings.SLACK_CHANNEL_ID,
+            text=message
+        )
+        print("Slack notification sent successfully!")
+        return True
+    except SlackApiError as e:
+        print(f"Error sending Slack notification: {e.response['error']}")
+        return False
+    except Exception as e:
+        print(f"Unexpected error when sending Slack notification: {e}")
+        traceback.print_exc()
+        return False
